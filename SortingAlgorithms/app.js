0f09c83d2a6a44de2d6098012757a23f69966fa5
Justin McCoy <31171668+Just-A-Engineer@users.noreply.github.com>
2021-02-17 16:54:10 -0700

    Updated app.js and index.html
    updated app.js and index.html to accommodate for new window prompt action

--- SortingAlgorithms/app.js
+++ SortingAlgorithms/app.js
@@ -2,12 +2,24 @@ let canvas = document.querySelector("#sortCanvas");
 let ctx = canvas.getContext("2d");
 let sortingArray = [];
 
+let resetButton = document.querySelector("#resetButton").addEventListener("click", function reset() {
+    drawRect(0, 0, canvas.width, canvas.height, "blue");
+    lineDraw();
+    console.log("hello from reset");
+  });
+
 function canvasSetup() {
   console.log("Hello from the otherside!");
 }
 
 function getRndmInt() {
   return Math.floor(Math.random() * canvas.height);
+}
+
+function reset() {
+
+  lineDraw();
+  console.log("hello from reset");
 }
 
 function swap(arr, x, y) {
@@ -17,7 +29,6 @@ function swap(arr, x, y) {
 }
 
 function bubbleSort() {
-  //Need to run an algorithm that loops through the sortingArray and sorts it in Bubble Sort fashion
   for(i = 0; i < sortingArray.length; i++) {
     for(j = 0; j < sortingArray.length-i-1; j++) {
       let a = sortingArray[j];
@@ -26,6 +37,19 @@ function bubbleSort() {
         swap(sortingArray, j, j + 1);
       }
     }
+  }
+}
+
+function insertSort() {
+  for(i = 0; i < sortingArray.length; i++) {
+    key = sortingArray[i]
+    
+    j = i-1
+    while(j >= 0 && key < sortingArray[j]) {
+      sortingArray[j +1] = sortingArray[j];
+      j-= 1
+    }
+    sortingArray[j + 1] = key
   }
 }
 
@@ -51,7 +75,13 @@ function lineDraw() {
     sortingArray[i] = getRndmInt();
   }
   
-  bubbleSort();
+  let sortingChoice = window.prompt("What sorting algorithm would you like to see? (Bubble, Insertion, Quick, Heap, and Merge)");
+  
+  if(sortingChoice === "bubble sort" || sortingChoice === "Bubble Sort") {
+    bubbleSort();
+  } else if(sortingChoice === "insertion sort" || sortingChoice === "Insertion Sort") {
+    insertSort();
+  }
   
   for(i = 0; i < sortingArray.length; i++) {
     draw(i, canvas.height, i, canvas.height - sortingArray[i], "white", 2);
