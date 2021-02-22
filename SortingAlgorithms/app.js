fbe8cba9b33dbbf3a4a963e2154687a3a4411055
Justin McCoy <31171668+Just-A-Engineer@users.noreply.github.com>
2021-02-21 17:40:04 -0700

    Added Quick Sort algorithm to app.js

--- SortingAlgorithms/app.js
+++ SortingAlgorithms/app.js
@@ -16,12 +16,6 @@ function getRndmInt() {
   return Math.floor(Math.random() * canvas.height);
 }
 
-function reset() {
-
-  lineDraw();
-  console.log("hello from reset");
-}
-
 function swap(arr, x, y) {
   let temp = arr[x]
   arr[x] = arr[y]
@@ -53,6 +47,33 @@ function insertSort() {
   }
 }
 
+function quickSort(arr, low, high) {
+  function partition(arr, low, high) {
+    let i = (low - 1);
+    let pivot = arr[high];
+    for(j = low; j < high; j++) {
+      if (arr[j] <= pivot) {
+        i = i + 1
+        temp = arr[i]
+        arr[i] = arr[j]
+        arr[j] = temp
+      }
+    }
+    temp2 = arr[i+1]
+    arr[i+1] = arr[high]
+    arr[high] = temp2
+      
+    return (i + 1)
+  }
+  
+  if(low < high) {
+    p = partition(arr, low, high);
+    
+    quickSort(arr, low, p - 1);
+    quickSort(arr, p + 1, high);
+  }
+}
+
 function drawRect(x, y, width, height, color) {
   ctx.beginPath();
   ctx.rect(x, y, width, height);
@@ -77,10 +98,12 @@ function lineDraw() {
   
   let sortingChoice = window.prompt("What sorting algorithm would you like to see? (Bubble, Insertion, Quick, Heap, and Merge)");
   
-  if(sortingChoice === "bubble sort" || sortingChoice === "Bubble Sort") {
+  if(sortingChoice === "bubble sort" || sortingChoice === "Bubble Sort" || sortingChoice === "bubble" || sortingChoice === "Bubble") {
     bubbleSort();
-  } else if(sortingChoice === "insertion sort" || sortingChoice === "Insertion Sort") {
+  } else if(sortingChoice === "insertion sort" || sortingChoice === "Insertion Sort" || sortingChoice === "insertion" || sortingChoice === "Insertion") {
     insertSort();
+  } else if(sortingChoice === "quick sort" || sortingChoice === "Quick Sort" || sortingChoice === "quick" || sortingChoice === "Quick") {
+    quickSort(sortingArray, 0, sortingArray.length-1);
   }
   
   for(i = 0; i < sortingArray.length; i++) {
